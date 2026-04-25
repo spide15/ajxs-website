2edd6573b0d9b57336de80a58025c5e7a5938692
spide15 <deepgada160@gmail.com>
2026-04-25 18:59:04 +0530

    base_path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ajxs-website/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
